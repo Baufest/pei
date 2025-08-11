432ab990b2235dc7d93a8110fcc83f722b211dd6
Fernando Lucchesi <flucchesi@baufest.com>
2025-08-11 10:41:55 -0300

    first commit

---
+++ src/main/java/com/pei/PeiApplication.java
@@ -0,0 +1,11 @@
+package com.pei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PeiApplication.class, args);
+    }
+}
