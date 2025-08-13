9f857defb6a1090869f0ee906be3322020293016
dborrellibau <dborrelli@baufest.com>
2025-08-13 12:28:09 -0300

    Revert "Fraude por chargeback y login desde multiples paises"

--- src/main/java/com/pei/repository/TransactionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pei.repository;
-
-import com.pei.domain.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-}
